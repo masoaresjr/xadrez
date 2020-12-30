08b8b8b4fe762b4d769322015ee3c82ab5836ecd
masoaresjr <masoaresjr@uol.com.br>
2020-12-30 19:30:47 -0300

    Remover repetição de código no movimento da Torre

--- Models/Pedras/Torre.py
+++ Models/Pedras/Torre.py
@@ -1,5 +1,6 @@
 from Models.Pedras.Pedra import Pedra
 import Models.Propriedades
+from itertools import chain
 
 
 class Torre(Pedra):
@@ -24,47 +25,52 @@ class Torre(Pedra):
         self.casa_inicial_tabuleiro = [self.coordenada_x, self.coordenada_y]
 
     def todos_movimentos(self, casas):
-        casas_posiveis = []
 
-        contador_x = 1
-        while self.coordenada_x + contador_x <= 7:
-            casa_destino = casas[self.coordenada_x + contador_x][self.coordenada_y]
-            if casa_destino.pedra is not None:
-                if casa_destino.pedra.cor != self.cor:
-                    casas_posiveis.append(casa_destino)
-                break
-            casas_posiveis.append(casa_destino)
-            contador_x += 1
+        movimentos_eixo_x = self.movimento_direcao_unica(True, casas)
+        movimentos_eixo_y = self.movimento_direcao_unica(False, casas)
 
-        contador_x = 1
-        while self.coordenada_x - contador_x >= 0:
-            casa_destino = casas[self.coordenada_x - contador_x][self.coordenada_y]
-            if casa_destino.pedra is not None:
-                if casa_destino.pedra.cor != self.cor:
-                    casas_posiveis.append(casa_destino)
-                break
-            casas_posiveis.append(casa_destino)
-            contador_x += 1
-
-        contador_y = 1
-        while self.coordenada_y + contador_y <= 7:
-            casa_destino = casas[self.coordenada_x][self.coordenada_y + contador_y]
-            if casa_destino.pedra is not None:
-                if casa_destino.pedra.cor != self.cor:
-                    casas_posiveis.append(casa_destino)
-                break
-            casas_posiveis.append(casa_destino)
-            contador_y += 1
-
-        contador_y = 1
-        while self.coordenada_y - contador_y >= 0:
-            casa_destino = casas[self.coordenada_x][self.coordenada_y - contador_y]
-            if casa_destino.pedra is not None:
-                if casa_destino.pedra.cor != self.cor:
-                    casas_posiveis.append(casa_destino)
-                break
-            casas_posiveis.append(casa_destino)
-            contador_y += 1
+        casas_posiveis = list(chain(movimentos_eixo_x, movimentos_eixo_y))
 
         return casas_posiveis
 
+    def movimento_direcao_unica(self, eixo_x, casas):
+        casas_posiveis = []
+
+        if eixo_x:
+            coordenada = self.__getattribute__("coordenada_x")
+        else:
+            coordenada = self.__getattribute__("coordenada_y")
+
+        contador = 1
+        while coordenada + contador <= 7:
+
+            if eixo_x:
+                casa_destino = casas[self.coordenada_x + contador][self.coordenada_y]
+            else:
+                casa_destino = casas[self.coordenada_x][self.coordenada_y + contador]
+
+            if casa_destino.pedra is not None:
+                if casa_destino.pedra.cor != self.cor:
+                    casas_posiveis.append(casa_destino)
+                break
+
+            casas_posiveis.append(casa_destino)
+            contador += 1
+
+        contador = 1
+        while coordenada - contador >= 0:
+
+            if eixo_x:
+                casa_destino = casas[self.coordenada_x - contador][self.coordenada_y]
+            else:
+                casa_destino = casas[self.coordenada_x][self.coordenada_y - contador]
+
+            if casa_destino.pedra is not None:
+                if casa_destino.pedra.cor != self.cor:
+                    casas_posiveis.append(casa_destino)
+                break
+
+            casas_posiveis.append(casa_destino)
+            contador += 1
+
+        return casas_posiveis
