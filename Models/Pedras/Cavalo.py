94e6d953aa4079ab5e2d5c12ecea9d495ea13f70
masoaresjr <masoaresjr@uol.com.br>
2020-12-31 17:11:15 -0300

    Método atualizar_possiveis_destinos cavalo

--- Models/Pedras/Cavalo.py
+++ Models/Pedras/Cavalo.py
@@ -23,10 +23,11 @@ class Cavalo(Pedra):
 
         self.casa_inicial_tabuleiro = [self.coordenada_x, self.coordenada_y]
 
-    def todos_movimentos(self, casas, fim_da_rodada):
+    def todos_movimentos(self, tabuleiro, fim_da_rodada):
         casas_possiveis = []
 
-        casas_destino = [self.movimentos([-1, 1], [2, -2], casas), self.movimentos([-2, 2], [1, -1], casas)]
+        casas_destino = [self.movimentos([-1, 1], [2, -2], tabuleiro, fim_da_rodada),
+                         self.movimentos([-2, 2], [1, -1], tabuleiro, fim_da_rodada)]
 
         for lista_de_casas in casas_destino:
             for casas in lista_de_casas:
@@ -34,19 +35,31 @@ class Cavalo(Pedra):
 
         return casas_possiveis
 
-    def movimentos(self, coordenadas_x, coordenadas_y, casas):
+    def movimentos(self, coordenadas_x, coordenadas_y, tabuleiro, fim_da_rodada):
         casas_possiveis = []
 
         for x in coordenadas_x:
             for y in coordenadas_y:
                 if 0 <= x + self.coordenada_x <= 7 and 0 <= y + self.coordenada_y <= 7:
-                    casa_destino = casas[x + self.coordenada_x][y + self.coordenada_y]
-                    if casa_destino.pedra is not None:
-                        if casa_destino.pedra.cor != self.cor:
-                            casas_possiveis.append(casa_destino)
-                        continue
+                    casa_destino = tabuleiro[x + self.coordenada_x][y + self.coordenada_y]
+
+                    if fim_da_rodada is False:
+                        if casa_destino.pedra is not None:
+                            if casa_destino.pedra.cor != self.cor:
+                                casas_possiveis.append(casa_destino)
+                            continue
                     casas_possiveis.append(casa_destino)
+
         return casas_possiveis
 
-    def atualizar_possiveis_destinos(self, casas):
-        pass
+    def atualizar_possiveis_destinos(self, tabuleiro):
+        for linhas in tabuleiro:
+            for casa in linhas:
+                if self in casa.possivel_destino_de:
+                    casa.possivel_destino_de.remove(self)
+
+        casas_destino = self.todos_movimentos(tabuleiro, True)
+
+        for casa_destino in casas_destino:
+            casa_destino.possivel_destino_de.append(self)
+
